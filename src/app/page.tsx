fd0b3f5bac22a1b7f4babcad4ed736dd4dc32535
Daniel Infante <danielinfantito61@gmail.com>
2023-06-06 22:58:23 -0500

    style: content body

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,15 @@
+import bgImage from '@assets/background.jpg'
 import styles from './page.module.scss'
 
 import { Header } from '@containers'
+
+const backgroundImage = `url(${bgImage.src})`
 
 function Page() {
   return (
     <main className={styles.main}>
       <Header />
-      <section className={styles.content}></section>
+      <section className={styles.content} style={{ backgroundImage }}></section>
       <footer className={styles.footer}></footer>
     </main>
   )
